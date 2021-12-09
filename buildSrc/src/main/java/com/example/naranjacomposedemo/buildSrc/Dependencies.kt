0f1fa185d480bd99829d83181be5a36b1d1d1bd7
Gastón Saillén <gsaillen95@gmail.com>
2021-12-09 11:22:34 -0300

    First commit

---
+++ buildSrc/src/main/java/com/example/naranjacomposedemo/buildSrc/Dependencies.kt
@@ -0,0 +1,88 @@
+package com.example.naranjacomposedemo.buildSrc
+
+object Versions {
+    const val ktlint = "0.43.0"
+}
+
+object Libs {
+    const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.3"
+
+    object Accompanist {
+        const val version = "0.21.3-beta"
+        const val insets = "com.google.accompanist:accompanist-insets:$version"
+        const val systemuicontroller = "com.google.accompanist:accompanist-systemuicontroller:$version"
+        const val flowlayouts = "com.google.accompanist:accompanist-flowlayout:$version"
+    }
+
+    object Kotlin {
+        private const val version = "1.5.31"
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
+        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
+        const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
+    }
+
+    object Coroutines {
+        private const val version = "1.5.2"
+        const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
+        const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
+        const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
+    }
+
+    object AndroidX {
+        const val coreKtx = "androidx.core:core-ktx:1.7.0"
+
+        object Compose {
+            const val snapshot = ""
+            const val version = "1.1.0-beta03"
+
+            const val foundation = "androidx.compose.foundation:foundation:${version}"
+            const val layout = "androidx.compose.foundation:foundation-layout:${version}"
+            const val ui = "androidx.compose.ui:ui:${version}"
+            const val uiUtil = "androidx.compose.ui:ui-util:${version}"
+            const val runtime = "androidx.compose.runtime:runtime:${version}"
+            const val material = "androidx.compose.material:material:${version}"
+            const val animation = "androidx.compose.animation:animation:${version}"
+            const val tooling = "androidx.compose.ui:ui-tooling:${version}"
+            const val iconsExtended = "androidx.compose.material:material-icons-extended:$version"
+            const val uiTest = "androidx.compose.ui:ui-test-junit4:$version"
+        }
+
+        object Activity {
+            const val activityCompose = "androidx.activity:activity-compose:1.4.0"
+        }
+
+        object Lifecycle {
+            const val viewModelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0-rc01"
+        }
+
+        object Navigation {
+            const val navigationCompose = "androidx.navigation:navigation-compose:2.4.0-beta02"
+        }
+
+        object ConstraintLayout {
+            const val constraintLayoutCompose =
+                "androidx.constraintlayout:constraintlayout-compose:1.0.0-rc01"
+        }
+
+        object Test {
+            private const val version = "1.4.0"
+            const val core = "androidx.test:core:$version"
+            const val runner = "androidx.test:runner:$version"
+            const val rules = "androidx.test:rules:$version"
+            object Ext {
+                private const val version = "1.1.2"
+                const val junit = "androidx.test.ext:junit-ktx:$version"
+            }
+            const val espressoCore = "androidx.test.espresso:espresso-core:3.2.0"
+        }
+    }
+
+    object JUnit {
+        private const val version = "4.13"
+        const val junit = "junit:junit:$version"
+    }
+
+    object Coil {
+        const val coilCompose = "io.coil-kt:coil-compose:1.3.2"
+    }
+}
